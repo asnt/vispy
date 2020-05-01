9c714b16fdabb6f590c4105f8cec8ec8462ea64c
Alexandre Saint <830589+asnt@users.noreply.github.com>
2020-05-01 20:43:06 +0200

    Add interactive control of the blending factor

--- examples/basics/scene/mesh_texture.py
+++ examples/basics/scene/mesh_texture.py
@@ -35,11 +35,34 @@ texture_filter = TextureFilter(texture, texcoords)
 mesh.attach(texture_filter)
 
 
+ctrl_down = False
+
 @canvas.events.key_press.connect
 def on_key_press(event):
+    global ctrl_down
     if event.key == "t":
         texture_filter.enabled = not texture_filter.enabled
         mesh.update()
+    elif event.key == "control":
+        ctrl_down = True
+
+
+@canvas.events.key_release.connect
+def on_key_release(event):
+    global ctrl_down
+    if event.key == "control":
+        ctrl_down = False
+
+
+@canvas.events.mouse_wheel.connect
+def on_mouse_wheel(event):
+    print(ctrl_down)
+    if ctrl_down:
+        factor = texture_filter.blending_factor + 0.05 * event.delta[1]
+        print(event.delta)
+        texture_filter.blending_factor = max(min(factor, 1.0), 0.0)
+        event.handled = True
+        event.blocked = True
 
 
 def attach_headlight(mesh, view, canvas):
